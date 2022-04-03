657c1e30d6c167909e36716b54d225a9c5a400b6
joyunji <@DESKTOP-3R7SINI>
2022-04-04 00:43:05 +0900

    환경세팅완료v1

---
+++ src/test/java/com/yunji/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunji.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
